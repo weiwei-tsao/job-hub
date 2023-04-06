ac883a55b310bdaae00b70d360e21202d808757f
weiwei-tsao <weiwei.tsao@hotmail.com>
2023-04-06 12:01:56 -0400

    feat: setup the tailwindcss

--- client/app/src/scenes/Home/index.tsx
+++ client/app/src/scenes/Home/index.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import logo from "../../assets/logo.png";
 
 const Home: React.FC = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div>
+      <main>
+        <p className="text-lg text-red-50">Landing Page</p>
+        <nav>
+          <img src={logo} alt="logo" />
+        </nav>
+
+        <div>
+          <div></div>
+          <img src="" alt="" />
+        </div>
+      </main>
+    </div>
+  );
 };
 export default Home;
---
+++ client/app/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "2.5rem",
+      "6xl": "3rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
